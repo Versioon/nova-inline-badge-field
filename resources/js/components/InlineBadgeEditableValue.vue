5656ec3bab8ab24e19c095f42cf12c87c0ac52b0
Tarvo Reinpalu <tarvo@versioon.ee>
2026-06-02 18:17:29 +0300

    Minor style fix

--- resources/js/components/InlineBadgeEditableValue.vue
+++ resources/js/components/InlineBadgeEditableValue.vue
@@ -13,16 +13,16 @@
 
         <svg
           v-if="editable"
-          class="nova-inline-badge-field-caret"
           xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          height="12"
+          width="12"
+          style="margin-left: 4px"
         >
-          <path
-            fill-rule="evenodd"
-            d="M5.23 7.21a.75.75 0 0 1 1.06.02L10 11.17l3.71-3.94a.75.75 0 1 1 1.08 1.04l-4.25 4.5a.75.75 0 0 1-1.08 0l-4.25-4.5a.75.75 0 0 1 .02-1.06z"
-            clip-rule="evenodd"
-          />
+          <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
         </svg>
       </span>
     </Badge>
